0c59f9712f60e56f89b4f6bf675699297e42b447
Erdal Karaer <karaer.erd@gmail.com>
2024-02-01 23:23:33 +0100

    Modification de l'email

--- src/pages/api/users.js
+++ src/pages/api/users.js
@@ -57,32 +57,21 @@ const handle = mw({
     async ({
       send,
       input: {
-        body: { userId, email, password },
+        body: { userId, email},
       },
       models: { UserModel },
     }) => {
-      //eslint-disable-next-line no-console
-      console.log("Received update request for userId:", userId)
-
       const user = await UserModel.query().findById(userId)
 
       if (!user) {
-        //eslint-disable-next-line no-console
-        console.log("User not found")
         send(false)
 
         return
       }
 
-      const [passwordHash, passwordSalt] = await hashPassword(password)
-
       await UserModel.query().findById(userId).patch({
         email,
-        passwordHash,
-        passwordSalt,
       })
-      //eslint-disable-next-line no-console
-      console.log("Profile updated successfully!")
       send(true)
     },
   ],
--- src/pages/your-profile.jsx
+++ src/pages/your-profile.jsx
@@ -1,8 +1,12 @@
+/* eslint-disable no-console */
+/* eslint-disable max-lines-per-function */
 import { useEffect, useState } from "react"
 
 const YourProfilePage = () => {
   const [user, setUser] = useState(null)
   const [loading, setLoading] = useState(true)
+  const [newEmail, setNewEmail] = useState("")
+  const [isEditing, setIsEditing] = useState(false)
 
   useEffect(() => {
     // Fetch user data from the GET API
@@ -25,6 +29,31 @@ const YourProfilePage = () => {
     fetchUser()
   }, [])
 
+  const handleEmailChange = async () => {
+    try {
+      const response = await fetch(`/api/users`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userId: user.id,
+          email: newEmail,
+        }),
+      })
+      const data = await response.json()
+
+      if (data.result && data.result[0] === true) {
+        setUser({ ...user, email: newEmail })
+        setIsEditing(false)
+      } else {
+        console.error("Error updating user email:", data.error)
+      }
+    } catch (error) {
+      console.error("Error updating user email", error)
+    }
+  }
+
   if (loading) {
     return <div>Loading...</div>
   }
@@ -37,6 +66,20 @@ const YourProfilePage = () => {
     <div>
       <h1>Your Profile</h1>
       <p>Email: {user.email}</p>
+
+      {isEditing ? (
+        <div>
+          <label>New Email:</label>
+          <input
+            type="email"
+            value={newEmail}
+            onChange={(e) => setNewEmail(e.target.value)}
+          />
+          <button onClick={handleEmailChange}>Save</button>
+        </div>
+      ) : (
+        <button onClick={() => setIsEditing(true)}>Edit Email</button>
+      )}
     </div>
   )
 }
