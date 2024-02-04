7337dc1e197af92a89b86dca8d1fe0d25ed86627
Erdal Karaer <karaer.erd@gmail.com>
2024-02-04 18:44:24 +0100

    J aurais du commencer plus tôt...

--- src/web/components/MainMenu.jsx
+++ src/web/components/MainMenu.jsx
@@ -1,14 +1,50 @@
+/* eslint-disable no-shadow */
+/* eslint-disable no-console */
+/* eslint-disable max-lines-per-function */
 import { useSession } from "@/web/components/SessionContext"
 import Link from "@/web/components/ui/Link"
 import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
 
 const MainMenu = ({ children: _, ...otherProps }) => {
   const { session, signOut } = useSession()
   const router = useRouter()
+  const [user, setUser] = useState(null)
   const handleSignOut = async () => {
     await signOut()
     router.push("/")
   }
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await fetch(`/api/users?id=${session.user.id}`)
+        const data = await response.json()
+
+        if (data && data.result && data.result.length > 0) {
+          const userWithMatchingId = data.result.find(
+            (user) => user.id === session.user.id,
+          )
+
+          if (userWithMatchingId) {
+            setUser(userWithMatchingId)
+          } else {
+            console.error("User not found")
+          }
+        } else {
+          console.error("No user in the database")
+        }
+      } catch (error) {
+        console.error("Error fetching user data", error)
+      } finally {
+        //
+      }
+    }
+
+    fetchUser()
+  }, [])
+
+  console.log("User:", user)
+  console.log("Session:", session)
 
   return (
     <nav {...otherProps}>
@@ -18,6 +54,13 @@ const MainMenu = ({ children: _, ...otherProps }) => {
             Home
           </Link>
         </li>
+        {user && user.role === "admin" && (
+          <li>
+            <Link href="/listUsersAdmin" styless>
+              List users
+            </Link>
+          </li>
+        )}
         {session ? (
           <>
             <li>
